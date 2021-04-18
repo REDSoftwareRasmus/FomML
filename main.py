7d11939b5c599da48fdfab71eb264d813a6c667f
Rasmus Englund <appdev@redge.se>
2021-04-18 13:15:50 +0200

    Add numpy learning + initial torch prediction

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import torch
-
-print("hello world")
-
+import numpy as np
 
 
+print("=======")
+a = torch.randint(1, 10, (3,3))
+print(a)
+b = torch.randint(1, 10, (3,3))
+#print(b)
+
+c = a.add_(5)
+print(c)
---
+++ pred.py
@@ -0,0 +1,17 @@
+import torch, torchvision
+
+model = torchvision.models.resnet18(pretrained=True)
+data = torch.rand(1, 3, 64, 64)
+labels = torch.rand(1, 1000)
+
+prediction = model(data)
+
+
+loss = (prediction - labels).sum()
+
+loss.backward() # backward pass
+
+optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+optim.step()
+
+print(optim)
